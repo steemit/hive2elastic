fa9629ca1af606d9e592ec48c51c7353e77a2a9c
ety001 <ety001@users.noreply.github.com>
2022-03-26 05:00:52 +0800

    Add healthcheck (#5)
    * change docker image base
    
    * add healthcheck bash

---
+++ healthcheck.sh
@@ -0,0 +1,27 @@
+#!/bin/ash
+
+
+c1=`ps aux | grep hive2elastic_post | grep -v grep | wc -l`
+c2=`ps aux | grep hive2elastic_account | grep -v grep | wc -l`
+
+if [[ $c1 -lt 1 ]]; then
+  echo Status: 500
+  echo Content-type:text/plain
+  echo
+  echo hive2elastic_post_not_healthy
+  exit 1
+fi
+
+if [[ $c2 -lt 1 ]]; then
+  echo Status: 500
+  echo Content-type:text/plain
+  echo
+  echo hive2elastic_account_not_healthy
+  exit 1
+fi
+
+echo Status: 200
+echo Content-type:text/plain
+echo
+echo healthcheck
+exit 0
